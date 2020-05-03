3f9447e805daba3ab79a237cfecc0d856242a507
lana-rodion <lana.rodion@gmail.com>
2020-05-03 22:24:56 +0200

    Board + Player + Weapon Class

--- js/weapons.js
+++ js/weapons.js
@@ -1,7 +1,12 @@
-class Weapons {
-    constructor(name, damage, display) {
+ export default class Weapon {
+    constructor(name, damage, skin, display) {
         this.name = name;
         this.damage = damage;
+        this.skin = skin;
         this.display = display;
+    }
+
+    insertWeapon(index) {
+        $(`#c${index}`).addClass(this.skin);
     }
 }
