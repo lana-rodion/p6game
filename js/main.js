1c8f4d03d1c0b6d98081c25730be06f96ac84b43
lana-rodion <lana.rodion@gmail.com>
2020-05-25 13:12:09 +0200

    update branch with findPosition function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-"use strict"
+"use strict";
 
 document.addEventListener("DOMContentLoaded", (event) => {
-	console.log("DOM entièrement chargé et analysé");
+	//console.log("DOM entièrement chargé et analysé");
 });
 
 
