684669e2e9afca28839194e21b7061caa43f9fd1
lana-rodion <lana.rodion@gmail.com>
2020-06-13 21:23:08 +0200

    remove step2 elements

--- js/players.js
+++ js/players.js
@@ -8,25 +8,6 @@ class Player {
         this.currentCell;
         this.defense = false;
     }
-
-    /* Cette méthode change la propriété player de l'ancienne et de la nouvelle case et modifie aussi le DOM pour la partie visuelle */
-    move(newCell) {
-        this.currentCell.player = null;
-        newCell.player = this;
-        this.currentCell = newCell;
-        $(".cell").removeClass("accessible");
-        $(`.${this.name}`).removeClass(this.name);
-        $(newCell.element).addClass(this.name);
-    }
-
-    isPlayerAround(cellsAround) {
-        for (let cell of cellsAround) {
-            if (cell.player !== null) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
 
 export let player1 = new Player("hero1");
