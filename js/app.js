8343449224afe73276bca03e7cde82b435a4efc5
lana-rodion <lana.rodion@gmail.com>
2020-06-13 22:50:00 +0200

    New feature players movements

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
 /*
- * Step 1 - Map generation
-    Start by randomly generating the game map
-    Each box can be either: empty or inaccessible (grayed background)
-    On the map, a limited number of weapons (4 maximum) will be randomly placed and can be collected by players who pass over them
-    Create at least 4 types of weapons in the game, with different damage
-    Each weapon has a name and an associated visual
-    The default players weapon must deal 10 points of damage
-    The placement of the two players on the map is random when the game is starting
-    Players cannot touch each other (they cannot be side by side) when the game is loading
+ * Step 2 - Player movements (turns)
+    On each turn, a player can move one to three spaces (horizontally or vertically) before complete his turn
+    He cannot cross an obstacle
+    If a player cross over a space containing a weapon, he leaves his current weapon in place and replaces it with the new one
+
 */
 
 import Game from "./game.js";
